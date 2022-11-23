7165a939ded276208b9f88fc4466efa67bcabbcb
David Lara <david.a.lara05@gmail.com>
2022-11-23 15:43:01 -0600

    created a sepeerate blog space for practice

---
+++ src/main/java/com/example/springbootpractice/controllers/BlogController.java
@@ -0,0 +1,20 @@
+package com.example.springbootpractice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blogs")
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String homePage(){
+        return "blogs/blog";
+    }
+
+
+
+
+
+}// End of BlogController
---
+++ src/main/java/com/example/springbootpractice/models/Blog.java
@@ -0,0 +1,4 @@
+package com.example.springbootpractice.models;
+
+public class Blog {
+}
