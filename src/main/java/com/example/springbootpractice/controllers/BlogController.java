9a3235139fea952b45e1b74a29a242dd037cf9f6
David Lara <david.a.lara05@gmail.com>
2022-11-25 21:12:08 -0600

    created method to post testing it

--- src/main/java/com/example/springbootpractice/controllers/BlogController.java
+++ src/main/java/com/example/springbootpractice/controllers/BlogController.java
@@ -1,15 +1,18 @@
 package com.example.springbootpractice.controllers;
 
+import com.example.springbootpractice.models.Blog;
 import com.example.springbootpractice.repositories.BlogsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/blogs")
 public class BlogController {
 
-//    Dao for blogrepo
+//    Dao for blog repo
     private final BlogsRepository blogDao;
 //    Constructor for blogDao
     public BlogController(BlogsRepository blogDao){
@@ -17,11 +20,21 @@ public class BlogController {
     }
 
 
+
+// This is the main page
     @GetMapping("/blog")
     public String homePage(){
         return "blogs/blog";
     }
 
+
+//    This creates the post
+    @PostMapping("/blog")
+    public String createPost(@RequestParam(name = "title")String title, @RequestParam(name = "body") String body){
+        Blog blog = new Blog(title,body);
+        blogDao.save(blog);
+        return "redirect:/blogs/allBlogs";
+    }
 
 
 
